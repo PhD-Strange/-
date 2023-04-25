478ae3bd9dd52364bf6d83479851ccbc05b881c6
Alex <agavshin25@gmail.com>
2023-04-25 06:59:15 +0300

    test

--- main.cpp
+++ main.cpp
@@ -4,14 +4,19 @@
 using namespace std;
 
 int main() {
-
+    string line;
+    ifstream in("input.txt");
+    if (in.is_open()) {
+        while (getline(in, line))
+            cout << line << endl;
+    }
+    in.close();
 
     ofstream out; // for recording
     out.open("output.txt");
     if (out.is_open())
         out << "Hello World?" << endl;
     out.close();
-    cout << "File has been written" << endl;
 
     return 0;
 };
