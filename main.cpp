6f5ffe45e58c70e221d62c7e4e025edc591f9d55
Alex <agavshin25@gmail.com>
2023-04-25 23:30:11 +0300

    night

--- main.cpp
+++ main.cpp
@@ -4,17 +4,28 @@
 #include <vector>
 using namespace std;
 
+int TIME_SCALE_M = 100;
+int SPACE_SCALE_N = 100;
+int TIME_SPLIT_T = 0.01;
+int SPACE_SPLIT_H = 0.03;
+
 class Node
 {
 private:
-    int m; // time
-    int n; // space // u{m, n} = u(mt, nh)
+    pair<int, int> u; // u{m, n} = u(mt, nh)
 public:
-    Node(int m, int n); // constructor
-    int GetTime() const; // get time-position
-    int GetSpace() const; // get space-position
+    Node(pair<int, int> u); // constructor
 };
 
+Node::Node(pair<int, int> u) {
+    do  
+        if (u.second < 0) {
+            u.second = u.second + SPACE_SCALE_N;
+        } else if (u.second > SPACE_SCALE_N * SPACE_SPLIT_H) {
+            u.second = u.second - SPACE_SCALE_N;
+        }
+    while (u.first > TIME_SCALE_M * TIME_SPLIT_T);
+};
 
 int main() {
     string line;
