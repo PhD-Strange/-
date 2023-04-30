ef2271fd01d1402b3ddae5f125e79d2ea45667ec
Alex <agavshin25@gmail.com>
2023-04-30 19:56:14 +0300

    aaaa

--- main.cpp
+++ main.cpp
@@ -2,31 +2,83 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <math.h>
 using namespace std;
 
-int TIME_SCALE_M = 100;
-int SPACE_SCALE_N = 100;
-int TIME_SPLIT_T = 0.01;
-int SPACE_SPLIT_H = 0.03;
+const int TIME_SCALE_M = 100;
+const int SPACE_SCALE_N = 100;
+const int TIME_SPLIT_T = 0.01;
+const int SPACE_SPLIT_H = 0.03;
 
 class Node
 {
 private:
-    int m; // u{m, n} = u(mT, nH)
-    int n;
+    int m_; // u{m, n} = u(mT, nH)
+    int n_;
+    double number;
 public:
-    Node(pair<int, int> u); // constructor
+    Node(int m, int n); // constructor
+    void setM(int m);
+    void setN(int n);
+    void setNumber(double z);
+    double getNumber();
 };
 
-Node::Node(pair<int, int> u) {
-    if (u.second < 0) {
-        u.second = u.second + SPACE_SCALE_N;
-    } else if (u.second > SPACE_SCALE_N * SPACE_SPLIT_H) {
-        u.second = u.second - SPACE_SCALE_N;
+Node::Node(int m, int n) {
+    if (n < 0) {
+        n_ = n + SPACE_SCALE_N;
+    } else if (n > SPACE_SCALE_N) {
+        n_ = n - SPACE_SCALE_N;
+    } else {
+        n_ = n;
     }
+    m_ = m;
 };
+
+void Node::setM(int m) {
+    if (m < 0) {
+        m_ = m + TIME_SCALE_M;
+    } else if (m > TIME_SCALE_M) {
+        m_ = m - TIME_SCALE_M;
+    } else {
+        m_ = m;
+    }
+}
+
+void Node::setN(int n) {
+    if (n < 0) {
+        n_ = n + SPACE_SCALE_N;
+    } else if (n > SPACE_SCALE_N) {
+        n_ = n - SPACE_SCALE_N;
+    } else {
+        n_ = n;
+    }
+}
+
+void Node::setNumber(double z){
+    number = z;
+}
+
+double Node::getNumber() {
+    return number;
+}
 
 int main() {
+    vector<Node> inits(SPACE_SCALE_N); // initial conditions
+    double velocity; // velocity = c > 0
+    double a; // shift
+
+    for (int i = 0; i < inits.size(); i++) {
+        double z = velocity/(2*pow(cosh(sqrt(velocity)*(i - a)/2), 2)); // solitonic initial conditions
+        inits.at(i).setM(0);
+        inits.at(i).setN(i);
+        inits.at(i).setNumber(z);
+    }
+
+    for (const Node& node: inits) {
+
+    }
+
     string line;
     ifstream in("input.txt");
     if (in.is_open()) {
