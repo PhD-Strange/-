eb06ebededc48c598cd08d63ac5fd166fb32863f
Alex <agavshin25@gmail.com>
2023-05-17 21:15:20 +0300

    exe

--- main.cpp
+++ main.cpp
@@ -10,74 +10,18 @@ const int SPACE_SCALE_N = 100;
 const int TIME_SPLIT_T = 0.01;
 const int SPACE_SPLIT_H = 0.03;
 
-class Node
-{
-private:
-    int m_; // u{m, n} = u(mT, nH)
-    int n_;
-    double number;
-public:
-    Node(int m, int n); // constructor
-    void setM(int m);
-    void setN(int n);
-    void setNumber(double z);
-    double getNumber();
-};
-
-Node::Node(int m, int n) {
-    if (n < 0) {
-        n_ = n + SPACE_SCALE_N;
-    } else if (n > SPACE_SCALE_N) {
-        n_ = n - SPACE_SCALE_N;
-    } else {
-        n_ = n;
-    }
-    m_ = m;
-};
-
-void Node::setM(int m) {
-    if (m < 0) {
-        m_ = m + TIME_SCALE_M;
-    } else if (m > TIME_SCALE_M) {
-        m_ = m - TIME_SCALE_M;
-    } else {
-        m_ = m;
-    }
-}
-
-void Node::setN(int n) {
-    if (n < 0) {
-        n_ = n + SPACE_SCALE_N;
-    } else if (n > SPACE_SCALE_N) {
-        n_ = n - SPACE_SCALE_N;
-    } else {
-        n_ = n;
-    }
-}
-
-void Node::setNumber(double z){
-    number = z;
-}
-
-double Node::getNumber() {
-    return number;
+double roll(vector<double>& v, unsigned int n) {
+    return v[n % v.size()];
 }
 
 int main() {
-    vector<Node> inits(SPACE_SCALE_N); // initial conditions
+    vector<double> inits; // initial conditions
     double velocity; // velocity = c > 0
     double a; // shift
 
-    for (int i = 0; i < inits.size(); i++) {
-        double z = velocity/(2*pow(cosh(sqrt(velocity)*(i - a)/2), 2)); // solitonic initial conditions
-        inits.at(i).setM(0);
-        inits.at(i).setN(i);
-        inits.at(i).setNumber(z);
-    }
+    vector<vector<double>> solution;
 
-    for (const Node& node: inits) {
-
-    }
+    
 
     string line;
     ifstream in("input.txt");
